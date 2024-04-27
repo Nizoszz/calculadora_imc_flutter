4f44735ce003a79a0e99f230335c7c7a0de77949
Andrew <andrewairamdasilva@gmail.com>
2024-04-27 10:11:18 -0300

    feat: init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:calculadora_imc_flutter/model/imc_model.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 import 'my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var documentsDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(documentsDirectory.path);
+  Hive.registerAdapter(IMCModelAdapter());
+
   runApp(const MyApp());
 }
 
